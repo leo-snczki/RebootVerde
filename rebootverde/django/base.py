5b503f925064a57341f233eb30783fb1fd4f50a4
leo-snczki <79675019+leo-snczki@users.noreply.github.com>
2026-02-19 22:56:04 +0000

    Set up basic local Docker environment with MySQL, Django, and phpMyAdmin
    to help develop locally, it is not using asgi or wsgi.

--- rebootverde/django/base.py
+++ rebootverde/django/base.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = 'rebootverde.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT', default='3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 

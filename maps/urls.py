2ac83fb816b358a08414d75740a454f87341cf74
leo-snczki <79675019+leo-snczki@users.noreply.github.com>
2026-03-12 11:07:16 +0000

    (WIP) Add filter with some brand that collects EEE
    - by now, it is only possible to choose all point or one brand

--- maps/urls.py
+++ maps/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("recycle-map/", views.recycle_map_view, name='maps'),
+    path('api/eco-points/', views.api_pontos_geojson, name='points-api'),
 ]
--- maps/views.py
+++ maps/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 from django.core.serializers import serialize
 from .models import PontoRecolha
+import json
+from django.http import JsonResponse
+
+BRANDS_REEE = ["Minipreço", "ALDI", "Minisom", "Fnac"] # dps coloco mais
 
 def recycle_map_view(request):
-    pontos = PontoRecolha.objects.filter(localidade="Lisboa")[:500]
-    pontos_geojson = serialize('geojson', pontos, geometry_field='geom', fields=('descricao', 'morada', 'localidade'))
-    
     return render(request, 'maps/recycle_map.html', {
-        'pontos_geojson': pontos_geojson
+        'brands': BRANDS_REEE
     })
+
+def api_pontos_geojson(request):
+    
+    brand_selecionada = request.GET.get('brand')
+    
+    pontos = PontoRecolha.objects.filter(localidade="Lisboa")
+
+    if brand_selecionada in BRANDS_REEE:
+        pontos = pontos.filter(descricao__icontains=brand_selecionada)
+    
+    
+    geojson_data = serialize(
+        'geojson', 
+        pontos[:500], # tem 475 em lisboa mas ok
+        geometry_field='geom',
+        fields=('descricao', 'morada', 'localidade')
+    )
+    
+    return JsonResponse(json.loads(geojson_data))

3e090778f376284990378d8b6065376d3c5a4c7d
leo-snczki <79675019+leo-snczki@users.noreply.github.com>
2026-03-18 12:13:01 +0000

    (Add civil parish filtering to recycle map: implement checkbox filters for civil parishes - implement logic to filter pins by selected civil parishes and update data loading functions - some civil parishes are not filtering like Arreiro, but I think it's because of the spelling in the database, so I will check that later)

--- maps/views.py
+++ maps/views.py
@@ -1,36 +1,51 @@
 from django.shortcuts import render
 from django.core.serializers import serialize
-from .models import PontoRecolha
+from .models import PontoRecolha, Freguesia
 import json
 from django.http import JsonResponse
 from django.db.models import Q
 
 BRANDS_REEE = ["Minipreço", "ALDI", "Minisom", "Fnac", "Primark", "Pingo Doce", "Canon", "Konica", "Nintendo", "Cepsa", "Worten", "Staples", "El Corte Inglês", "Decathlon", "Leroy Merlin", "Auchan", "Junta de Freguesia", "Hotel", "Lidl", "ALE-HOP" ] # dps coloco mais
 
+CIVIL_PARISHES = ["Ajuda", "Alcântara", "Alvalade", "Arreiro", "Arroios", "Avenidas Novas", "Beato", "Belém", "Benfica", "Campo de Ourique", "Campolide", "Carnide", "Estrela", "Lumiar", "Marvila", "Misericórdia", "Olivais", "Parque das Nações", "Penha de França", "Santa Clara", "Santa Maria Maior", "Santo António", "São Domingos de Benfica", "São Vicente"]
 def recycle_map_view(request):
     return render(request, 'maps/recycle_map.html', {
-        'brands': BRANDS_REEE
+        'brands': BRANDS_REEE,
+        'parishes': CIVIL_PARISHES
     })
 
 def api_pins_geojson(request):
-    selected_brandscionadas = request.GET.getlist('brand')
+    selected_brands = request.GET.getlist('brand')
     search = request.GET.get('search')
-    
+    selected_parishes = request.GET.getlist('parish')
+
     pins = PontoRecolha.objects.filter(localidade__iexact="lisboa")
 
-    # Se houver marcas selecionadas, construímos uma consulta dinâmica
-    # Usamos o objeto Q para acumular filtros com o 'OR'
-    if selected_brandscionadas:
+    if search:
+        pins = pins.filter(descricao__icontains=search)
+
+    if selected_brands:
         brand_filter = Q()
-        
-        for brand in selected_brandscionadas:
+        for brand in selected_brands:
             brand_filter |= Q(descricao__icontains=brand)
         
         pins = pins.filter(brand_filter)
-    
-    if search:
-        pins = pins.filter(descricao__icontains=search)
+
+    if selected_parishes:
+        freguesias_selecionadas = Freguesia.objects.filter(
+            concelho='Lisboa', 
+            nome__in=selected_parishes
+        )
         
+        if freguesias_selecionadas.exists():
+            parish_filter = Q()
+            for freguesia in freguesias_selecionadas:
+                parish_filter |= Q(geom__within=freguesia.geom)
+            
+            pins = pins.filter(parish_filter)
+        else:
+            pins = pins.none()
+            
     geojson_data = serialize(
         'geojson', 
         pins[:500], # tem 475 em lisboa mas ok

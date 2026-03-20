dcba23e8bb1fe31892a58f1391794ffa74e0d315
leo-snczki <79675019+leo-snczki@users.noreply.github.com>
2026-03-20 11:05:16 +0000

    update civil parishes list in recycle map: add missing parishes
    - add old civil parishes due to the file that we are using, I can merge later with updated parishes

--- maps/views.py
+++ maps/views.py
@@ -7,7 +7,19 @@ from django.db.models import Q
 
 BRANDS_REEE = ["Minipreço", "ALDI", "Minisom", "Fnac", "Primark", "Pingo Doce", "Canon", "Konica", "Nintendo", "Cepsa", "Worten", "Staples", "El Corte Inglês", "Decathlon", "Leroy Merlin", "Auchan", "Junta de Freguesia", "Hotel", "Lidl", "ALE-HOP" ] # dps coloco mais
 
-CIVIL_PARISHES = ["Ajuda", "Alcântara", "Alvalade", "Arreiro", "Arroios", "Avenidas Novas", "Beato", "Belém", "Benfica", "Campo de Ourique", "Campolide", "Carnide", "Estrela", "Lumiar", "Marvila", "Misericórdia", "Olivais", "Parque das Nações", "Penha de França", "Santa Clara", "Santa Maria Maior", "Santo António", "São Domingos de Benfica", "São Vicente"]
+CIVIL_PARISHES = [
+    "Ajuda", "Alcântara", "Alto Do Pina", "Alvalade", "Ameixoeira", "Anjos", 
+    "Beato", "Benfica", "Campo Grande", "Campolide", "Carnide", "Castelo", 
+    "Charneca", "Coração De Jesus", "Encarnação", "Graça", "Lapa", "Lumiar", 
+    "Madalena", "Mártires", "Marvila", "Mercês", "Nossa Senhora De Fátima", 
+    "Pena", "Penha De França", "Prazeres", "Sacramento", "Santa Catarina", 
+    "Santa Engrácia", "Santa Isabel", "Santa Justa", "Santa Maria De Belém", 
+    "Santa Maria Dos Olivais", "Santiago", "Santo Condestável", "Santo Estêvão", 
+    "Santos-O-Velho", "São Cristóvão E São Lourenço", "São Domingos De Benfica", 
+    "São Francisco Xavier", "São João", "São João De Brito", "São João De Deus", 
+    "São Jorge De Arroios", "São José", "São Mamede", "São Miguel", "São Nicolau", 
+    "São Paulo", "São Sebastião Da Pedreira", "São Vicente De Fora", "Sé", "Socorro"
+]
 def recycle_map_view(request):
     return render(request, 'maps/recycle_map.html', {
         'brands': BRANDS_REEE,

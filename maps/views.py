b6a4ee827712102278c913e5ed5bc32346bc9358
leo-snczki <79675019+leo-snczki@users.noreply.github.com>
2026-03-26 10:06:14 +0000

    Refactor brand and parish lists for improved readability and maintainability

--- maps/views.py
+++ maps/views.py
@@ -5,19 +5,22 @@ import json
 from django.http import JsonResponse
 from django.db.models import Q
 
-BRANDS_REEE = ["Minipreço", "Continente", "ALDI", "Minisom", "Fnac", "Primark", "Pingo Doce", "Canon", "Konica", "Nintendo", "Cepsa", "Worten", "Staples", "El Corte Inglês", "Decathlon", "Leroy Merlin", "Auchan", "Junta de Freguesia", "Hotel", "Lidl", "ALE-HOP" ] # dps coloco mais
+BRANDS_REEE = [
+    "Minipreço", "Continente", "ALDI", "Minisom", "Fnac", "Primark", "Pingo Doce", "Canon", "Konica", "Nintendo", "Cepsa", "Worten",
+    "Staples", "El Corte Inglês", "Decathlon", "Leroy Merlin", "Auchan", "Junta de Freguesia", "Hotel", "Lidl", "ALE-HOP"
+]
 
 CIVIL_PARISHES = [
-    "Ajuda", "Alcântara", "Alto Do Pina", "Alvalade", "Ameixoeira", "Anjos", 
-    "Beato", "Benfica", "Campo Grande", "Campolide", "Carnide", "Castelo", 
-    "Charneca", "Coração De Jesus", "Encarnação", "Graça", "Lapa", "Lumiar", 
-    "Madalena", "Mártires", "Marvila", "Mercês", "Nossa Senhora De Fátima", 
-    "Pena", "Penha De França", "Prazeres", "Sacramento", "Santa Catarina", 
-    "Santa Engrácia", "Santa Isabel", "Santa Justa", "Santa Maria De Belém", 
-    "Santa Maria Dos Olivais", "Santiago", "Santo Condestável", "Santo Estêvão", 
-    "Santos-O-Velho", "São Cristóvão E São Lourenço", "São Domingos De Benfica", 
-    "São Francisco Xavier", "São João", "São João De Brito", "São João De Deus", 
-    "São Jorge De Arroios", "São José", "São Mamede", "São Miguel", "São Nicolau", 
+    "Ajuda", "Alcântara", "Alto Do Pina", "Alvalade", "Ameixoeira", "Anjos",
+    "Beato", "Benfica", "Campo Grande", "Campolide", "Carnide", "Castelo",
+    "Charneca", "Coração De Jesus", "Encarnação", "Graça", "Lapa", "Lumiar",
+    "Madalena", "Mártires", "Marvila", "Mercês", "Nossa Senhora De Fátima",
+    "Pena", "Penha De França", "Prazeres", "Sacramento", "Santa Catarina",
+    "Santa Engrácia", "Santa Isabel", "Santa Justa", "Santa Maria De Belém",
+    "Santa Maria Dos Olivais", "Santiago", "Santo Condestável", "Santo Estêvão",
+    "Santos-O-Velho", "São Cristóvão E São Lourenço", "São Domingos De Benfica",
+    "São Francisco Xavier", "São João", "São João De Brito", "São João De Deus",
+    "São Jorge De Arroios", "São José", "São Mamede", "São Miguel", "São Nicolau",
     "São Paulo", "São Sebastião Da Pedreira", "São Vicente De Fora", "Sé", "Socorro"
 ]
 
@@ -26,6 +29,7 @@ def recycle_map_view(request):
         'brands': BRANDS_REEE,
         'parishes': CIVIL_PARISHES
     })
+
 
 def api_pins_geojson(request):
     selected_brands = request.GET.getlist('brand')
@@ -41,29 +45,29 @@ def api_pins_geojson(request):
         brand_filter = Q()
         for brand in selected_brands:
             brand_filter |= Q(descricao__icontains=brand)
-        
+
         pins = pins.filter(brand_filter)
 
     if selected_parishes:
         freguesias_selecionadas = Freguesia.objects.filter(
-            concelho='Lisboa', 
+            concelho='Lisboa',
             nome__in=selected_parishes
         )
-        
+
         if freguesias_selecionadas.exists():
             parish_filter = Q()
             for freguesia in freguesias_selecionadas:
                 parish_filter |= Q(geom__within=freguesia.geom)
-            
+
             pins = pins.filter(parish_filter)
         else:
             pins = pins.none()
-            
+
     geojson_data = serialize(
-        'geojson', 
+        'geojson',
         pins[:1200],
         geometry_field='geom',
         fields=('descricao', 'morada', 'localidade', 'codigo_pos')
     )
-    
+
     return JsonResponse(json.loads(geojson_data))

e592a0ab74c4bc4ba915cb7b2aa682973f848f62
leo-snczki <79675019+leo-snczki@users.noreply.github.com>
2026-04-19 19:58:49 +0000

    Add password reset functionality with corresponding templates and URL routes

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,26 @@
 from django.urls import path, include
-from . import views  
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path("accounts/register/", views.register_user, name="register"),
     path("accounts/verify/", views.verify_code, name="verify_code"),
+    path("accounts/password_reset/", auth_views.PasswordResetView.as_view(
+        template_name="users/password_reset_form.html",
+        email_template_name="users/password_reset_email.html"
+    ), name="password_reset"),
+
+    path("accounts/password_reset/done/", auth_views.PasswordResetDoneView.as_view(
+        template_name="users/password_reset_done.html"
+    ), name="password_reset_done"),
+
+    path("accounts/reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+        template_name="users/password_reset_confirm.html"
+    ), name="password_reset_confirm"),
+
+    path("accounts/reset/done/", auth_views.PasswordResetCompleteView.as_view(
+        template_name="users/password_reset_complete.html"
+    ), name="password_reset_complete"),
+
     path("accounts/", include("django.contrib.auth.urls")),
 ]
